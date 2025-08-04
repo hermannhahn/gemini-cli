6887fab2eda5e86480193738851e48873f660862
Hermann Hahn <hermann.h.hahn@gmail.com>
2025-08-04 00:36:29 -0300

    chore: Release v0.1.27

--- scripts/tests/get-release-version.test.js
+++ scripts/tests/get-release-version.test.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
 import { getReleaseVersion } from '../get-release-version';
 import { execSync } from 'child_process';
