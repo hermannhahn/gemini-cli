d559b273b3b0627cf21d791dae37ba92f064c1d1
Hermann Hahn <hermann.h.hahn@gmail.com>
2025-08-03 23:29:01 -0300

    chore: Release v0.1.27

--- scripts/update_package_versions.js
+++ scripts/update_package_versions.js
@@ -10,11 +10,19 @@ import { execSync } from 'child_process';
 
 const rootPackageJsonPath = join(process.cwd(), 'package.json');
 const cliPackageJsonPath = join(process.cwd(), 'packages/cli/package.json');
+const currentBranch = execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim();
+
+if (currentBranch !== 'hermannhahn/main' && currentBranch !== 'main') {
+  console.error('This script can only be run on the main branch.');
+  process.exit(1);
+}
 
 const newVersion = process.argv[2];
 
 if (!newVersion) {
-  console.error('Usage: node scripts/update_package_versions.js <new_version>');
+  console.error('Usage: npm run publish <new_version>');
   process.exit(1);
 }
 
@@ -30,15 +38,25 @@ try {
   updatePackageJson(rootPackageJsonPath, newVersion);
   updatePackageJson(cliPackageJsonPath, newVersion);
 
+  // install and compile
+  console.log('Installing...');
+  execSync('npm install');
+  console.log('Compiling...');
+  execSync('npm run build');
+  console.log('Succefully installed and compiled Geminid CLI v' + newVersion);
+
   // Add changes to git
-  execSync('git add package.json packages/cli/package.json');
+  execSync('git add .');
   console.log('Added package.json files to git staging area.');
 
   // Commit the version update
-  execSync(`git commit -m "chore: Release ${newVersion}"`);
+  execSync(`git commit -m 'chore: Release v${newVersion}'`);
   console.log(`Created commit for version ${newVersion}.`);
 
   console.log('Version update and tagging complete.');
+  console.log('Publishing...');
+  execSync('git push');
+  console.log('Published successfully.');
 } catch (error) {
   console.error('Error during version update:', error.message);
   process.exit(1);
